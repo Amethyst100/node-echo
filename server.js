196b963427ce8794ce80388c29072cac4c3873a1
Amethyst100 <asadwaleed93@gmail.com>
2016-12-29 15:18:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -73,6 +73,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/info', function (req, res) {
+  res.send('Hello! Test Info');
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
